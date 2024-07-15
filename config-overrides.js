7f44645c257fd647c039bee128ee2e88904db4c9
PerrkMP <perrk.mp@gmail.com>
2024-07-15 14:12:47 +0300

    fix

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+    config.resolve.fallback = {
+        util: require.resolve('util/'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert/')
+    };
+
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer'],
+        }),
+    );
+
+    return config;
+};
